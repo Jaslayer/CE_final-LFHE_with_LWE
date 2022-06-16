c0ee031caa7298fd4a8478368af74591358a2a4e
yourname0729 <qazwexsdc@gmail.com>
2022-06-16 15:56:12 +0800

    lfhe

--- lfhe.py
+++ lfhe.py
@@ -7,30 +7,37 @@ class ApproxEigenvecHomorphicSystem:
         self.lamb, self.L = lamb, L
 
         self.n = lamb
-        self.q = self.n
-        self.kappa = np.ceil(np.log2(self.q))
-        self.chi = lambda: 0 # TODO
+        # self.q = self.n
+        self.q = 97
+        self.kappa = int(np.ceil(np.log2(self.q)))
+        # self.chi = lambda: 0 # TODO
+        self.chi = lambda: np.random.randint(5)
         self.m = self.n * self.kappa
-        self.l = np.floor(np.log2(self.q)) + 1
+        self.l = int(np.floor(np.log2(self.q)) + 1)
         self.N = (self.n + 1) * self.l
         pass
 
     def keyGen(self):
         t = np.random.randint(self.q, size = self.n)
-        sk = np.append([1], -t)
+        sk = np.append([1], -t) % self.q
 
         B = np.random.randint(self.q, size = (self.m, self.n))
         e = np.array([self.chi() for _ in range(self.m)])
-        b = np.matmul(B, t) + e
+        b = (np.matmul(B, t) + e) % self.q
         A = np.concatenate([B, b[:, np.newaxis]], axis = 1)
         return sk, A
         pass
     
     def enc(self, pk, mu):
         r = np.random.randint(2, size = (self.N, self.m))
-        return Flatten(mu * np.identity(self.N) + BitDecomp(np.matmul(r, pk)))
+        return Flatten(mu * np.identity(self.N) + BitDecomp(np.matmul(r, pk), self.q))
         pass
+
     def dec(self, sk, C):
+        i = self.l - 2
+        v = Powerof2(sk)
+        x_i = np.inner(C[i, :], v)
+        return x_i / v[i]
         pass
 
 if __name__ == '__main__':
